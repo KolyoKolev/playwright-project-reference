0042bf410e1563f89a9cacaa76a3b45a455dbc02
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-05-13 09:12:03 +0300

    add new test

--- tests/upload_files.spec.js
+++ tests/upload_files.spec.js
@@ -3,6 +3,8 @@ const expect = require('chai').expect;
 let page, browser, context;
 
 const helloWorldFilePath = './tests/docs/hello_world.txt';
+const helloWorld2FilePath = './tests/docs/hello_world_2.txt';
+const dragAndDropArea = '#drag-drop-upload';
 
 describe('Files upload', () => {
   beforeEach('should load test page', async () => {
@@ -17,7 +19,14 @@ describe('Files upload', () => {
     page.on('filechooser', async (fileChooser) => {
       await fileChooser.setFiles(helloWorldFilePath);
     });
-    await page.click('#drag-drop-upload');
+    await page.click(dragAndDropArea);
+  });
+
+  it('should drag and drop multiple files', async () => {
+    page.on('filechooser', async (fileChooser) => {
+      await fileChooser.setFiles([helloWorldFilePath, helloWorld2FilePath]);
+    });
+    await page.click(dragAndDropArea);
   });
 
   afterEach('should close everything', async () => {
